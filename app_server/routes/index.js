fadf6924e701f7abe3f9be531a4c95b56521dff3
Anuraguniversity <auakshhay23@gmail.com>
2025-08-02 11:06:02 +0530

    Save changes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AU-Redio' });
+  res.render('index', { title: 'MoodyO' });
 });
 
 module.exports = router;
